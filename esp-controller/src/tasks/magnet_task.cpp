60ba23e69ae317c266b8e6de50f0c32eb7419f25
Logan Underwood <109767467+soulFood5632@users.noreply.github.com>
2026-02-06 23:06:29 -0800

    Logan set up main state (#1)
    * created temporary logic in the magnet reads + modified the types of magnet addresses
    
    * renamed some things
    
    * temp

--- esp-controller/src/tasks/magnet_task.cpp
+++ esp-controller/src/tasks/magnet_task.cpp
@@ -2,5 +2,28 @@
 #include "../core/global_state.h"
 
 void startMagnetTask() {
-    // TODO: implement magnet actuation loop reading control outputs from global state
+    GlobalState& state = GlobalState::instance();
+
+
+
+    while (true) {
+
+        // this gets a list of all non-zero control outputs 
+        // TODO: consider an optimization around setting a flag when the control outputs have changed instead of polling
+        const auto& new_control_outputs = state.getLatestControl();
+
+        // TODO: zero the previous magnets before new values. 
+        for (const auto& output : new_control_outputs) {
+            int magnetId = output.magnetId;
+            float current_value = output.current_value;
+
+            PWMAddress pwmAddr = state.getPWMAddress(magnetId);
+            ADCAddress adcAddr = state.getADCAddress(magnetId);
+
+            // Read the current and do whatever you need to to mantain the current at the given magnets at the right value.
+        }
+    }
+    
 }
+
+
